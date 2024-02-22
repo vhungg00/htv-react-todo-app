475917a446cd1f04d1799cdb58f0e8c7dfc83f2c
hungtv <tavanhunglc2000@gmail.com>
2024-02-22 17:53:09 +0700

    update 2402

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from '@/styles/modules/app.module.scss';
+import {Helmet} from "react-helmet-async";
+import {PageTitle} from "@/component/PageTitle";
 
 const App: React.FC = () => {
     return (
         <>
+            <Helmet>
+                <title>Todo app</title>
+            </Helmet>
             <div className='container'>
-
+                <PageTitle>Todo list</PageTitle>
             </div>
             <div className={styles.wrapper}>
 f
