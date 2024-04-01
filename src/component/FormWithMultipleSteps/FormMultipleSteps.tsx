f9711853baf4b4663251de23b6de6030ef9da9e5
vhungg00 <81247920+vhungg00@users.noreply.github.com>
2024-04-01 23:38:55 +0700

    latest_night

--- src/component/FormWithMultipleSteps/FormMultipleSteps.tsx
+++ src/component/FormWithMultipleSteps/FormMultipleSteps.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Helmet } from "react-helmet-async";
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, useFormState } from 'react-hook-form';
 
 import { Container } from "../Container";
+
+import './FormMultipleSteps.scss';
 
 export type FormRegister = {
     fname: string,
@@ -21,7 +23,17 @@ export type FormRegister = {
 
 export const FormWithMultipleSteps: React.FC = () => {
 
-    const { register } = useForm<FormRegister>({ shouldUnregister: true })
+    const { register, handleSubmit, control, watch } = useForm<FormRegister>({ shouldUnregister: true });
+
+    const firstNameValue: string = watch('fname');
+    
+    const { errors, isDirty, isValid } = useFormState({control});
+
+    console.log('errors', errors['fname'])
+
+    const goNext:SubmitHandler<FormRegister> = useCallback((data) => {
+        console.log(data);
+    },[]);
 
     return (
         <>
@@ -29,9 +41,41 @@ export const FormWithMultipleSteps: React.FC = () => {
                 <title>Form with multiple steps</title>
             </Helmet>
             <Container>
-                <h2 className='heading'>Register</h2>
-                <form id="regForm">
-                    <input {...register('fname')} />
+                <form id="regForm" onSubmit={handleSubmit(goNext)}>
+                <h1 className='heading'>Register</h1>
+                    <div className="tab">Name:
+                    <dl className="form-group">
+                        <dt className="form-label">
+                            First name
+                        </dt>
+                            <dd className="form-control">
+                                <label htmlFor="firstName">
+                                <input 
+                                    id="firstName"
+                                    className="input-control"
+                                    placeholder="First name..." {...register('fname', {
+                                    required: { value: true, message:'Vui lòng nhập trường này'}
+                                    })}
+                                    data-error={!!errors['fname']}
+                                    data-complete={!!firstNameValue}
+
+                                /></label>
+                            </dd>
+                    </dl>
+                        <label htmlFor="lastName">
+                            Last name:
+                            <input id="lastName" placeholder="Last name..." {...register('lname')} /></label>
+                    </div>
+                    <div className="tab">Contact Info:
+                        <label><input placeholder="E-mail..." {...register('email')} /></label>
+                        <label><input placeholder="Phone..." {...register('phone')} /></label>
+                    </div>
+                    <div style={{overflow: 'auto'}}>
+                        <div style={{float: 'right'}}>
+                        <button type="button" id="prevBtn">Previous</button>
+                        <button type="submit" id="nextBtn">Next</button>
+                        </div>
+                    </div>  
                 </form>
             </Container>
         </>
