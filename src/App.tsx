3eee1a3dde54e83f27efc1c9c783a5a58925e8e9
hungtv <tavanhunglc2000@gmail.com>
2024-07-05 11:50:46 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,71 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import loadable from '@loadable/component';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import loadable from '@loadable/component'
 
 // import { ScrollToTop } from './components/ScrollToTop';
-import { AppHeader } from './components/AppHeader';
-import { ScreenUrlPath } from './typing/ScreenUrlPath';
-import { AppFooter } from './components/AppFooter';
-import { ScrollToTop } from './components/ScrollToTop/ScrollToTop';
+import { AppHeader } from './components/AppHeader'
+import { ScreenUrlPath } from './typing/ScreenUrlPath'
+import { AppFooter } from './components/AppFooter'
+import { ScrollToTop } from './components/ScrollToTop/ScrollToTop'
+// import { ScrollTopNav } from './components/elements/ScrollTopNav'
 
 const Home = loadable(() => import('@/pages/Home'), {
-    resolveComponent: (components) => components.Home
+  resolveComponent: components => components.Home,
 })
 
 const LuckyGame = loadable(() => import('@/pages/LuckyWheelGame'), {
-    resolveComponent: (components) => components.LuckyWheelGame
-});
+  resolveComponent: components => components.LuckyWheelGame,
+})
 
-const FormMultiple = loadable(() => import('@/components/FormWithMultipleSteps'), {
-    resolveComponent: (components) => components.FormWithMultipleSteps
-});
+const FormMultiple = loadable(
+  () => import('@/components/FormWithMultipleSteps'),
+  {
+    resolveComponent: components => components.FormWithMultipleSteps,
+  },
+)
 
 const AllProduct = loadable(() => import('@/pages/AllProduct'), {
-    resolveComponent: (components) => components.AllProduct
-});
+  resolveComponent: components => components.AllProduct,
+})
 
 const Partners = loadable(() => import('@/components/PartnersSection'))
 
-const PageNotFound = loadable(() => import('@/components/404'));
+const PageNotFound = loadable(() => import('@/components/404'))
+
+const CaculatorYearMonth = loadable(
+  () => import('@/pages/CaculatorYearMonth'),
+  {
+    resolveComponent: component => component.Caculator,
+  },
+)
 
 const App: React.FC = () => {
-    return (
-        <div>
-            <Router>
-                <AppHeader />
-                {/* <ScrollToTop /> */}
-                <Routes>
-                    <Route path={ScreenUrlPath.Root} element={<Home />} />
-                    <Route path={ScreenUrlPath.LuckyWheelGame} element={<LuckyGame />} />
-                    <Route path={ScreenUrlPath.FormWithMultipleSteps} element={<FormMultiple />} />
-                    <Route path={ScreenUrlPath.AllProduct} element={<AllProduct />} />
-                    <Route path='*' element={<PageNotFound />} />
-                </Routes>
-                <Partners />
-                <AppFooter />
-                <ScrollToTop scrollY={300} />
-            </Router>
-        </div>
-    );
-};
+  return (
+    <div>
+      <Router>
+        <AppHeader />
+        {/* <ScrollToTop /> */}
+        {/* <ScrollTopNav /> */}
+        <Routes>
+          <Route path={ScreenUrlPath.Root} element={<Home />} />
+          <Route path={ScreenUrlPath.LuckyWheelGame} element={<LuckyGame />} />
+          <Route
+            path={ScreenUrlPath.FormWithMultipleSteps}
+            element={<FormMultiple />}
+          />
+          <Route path={ScreenUrlPath.AllProduct} element={<AllProduct />} />
+          <Route
+            path={ScreenUrlPath.CaculatorYearMonthYear}
+            element={<CaculatorYearMonth />}
+          />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Partners />
+        <AppFooter />
+        <ScrollToTop scrollY={300} />
+      </Router>
+    </div>
+  )
+}
 
-export default App;
+export default App
--- src/typing/ScreenUrlPath.ts
+++ src/typing/ScreenUrlPath.ts
@@ -1,7 +1,8 @@
 export const ScreenUrlPath = {
-    Root: '/',
-    LuckyWheelGame: 'lucky-wheel-game',
-    FormWithMultipleSteps: 'form-multiple',
-    Todo: 'todo',
-    AllProduct: 'all-products'
-} as const;
+  Root: '/',
+  LuckyWheelGame: 'lucky-wheel-game',
+  FormWithMultipleSteps: 'form-multiple',
+  Todo: 'todo',
+  AllProduct: 'all-products',
+  CaculatorYearMonthYear: 'caculator-year-month-year',
+} as const
